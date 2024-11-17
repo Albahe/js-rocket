22a62b16c40101f805858c7c8cba99a5c6d93239
Alba <albahuer@rocketmail.com>
2024-11-17 19:01:55 +0100

    It should work

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-var doCoolStuff = function () {
-	var currentClassName = document.getElementById("cool").className;
-
-	if (currentClassName == "cool") {
-		document.getElementById("cool").className = "cool red";
-	} else {
-		document.getElementById("cool").className = "cool";
-	}
-}
-
-/*
-var RedSquare = function () {
-	var currentClassName = document.getElementById("RedSquare").className;
-	if (currentClassName == "RedSquare") {
-		document.getElementById("RedSquare").className = "RedSquare YellowCircle";
-	} else {
-		document.getElementById("RedSquare").className = "RedSquare";
-	}
-}
-*/
-
 function switchClass () {
 	var currentClassName = document.getElementById("idObject").className;
 	if (currentClassName == "RedSquare") {
@@ -27,37 +6,3 @@ function switchClass () {
 		document.getElementById("idObject").className = "RedSquare";
 	}
 }
-
-
-var sayMyName = function (name) {
-	alert("My name is: "+name);
-}
-
-var car = {
-		make: "VW",
-		type: "Polo",
-		color: "blue",
-		isTurnedOn: false,
-		numberOfWheels: 4,
-		seats: ["seat 1",
-			"seat 2",
-			"seat 3",
-			"seat 4"],
-		turnOn: function () {
-			this.isTurnedOn = true;
-		},
-		fly: function () {
-			alert("fly")
-		},
-		switchCar: function (isOn) {
-			console.log("turn car "+isOn)
-			if (isOn == true) {
-			this.isTurnedOn = true;
-			} else {
-				this.isTurnedOn = false;
-			}
-		}
-
-};
-
-console.log('hello there friends!');
